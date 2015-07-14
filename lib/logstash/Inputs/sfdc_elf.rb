a3f8993aed1731f22358828e825879f7d3a99e2c
Mohammed Islam <mislam@mislam-ltm.internal.salesforce.com>
2015-07-14 09:59:16 -0700

    Refactored code into smaller private methods, But currently have a bug when running it on adams org :(

--- lib/logstash/Inputs/sfdc_elf.rb
+++ lib/logstash/Inputs/sfdc_elf.rb
@@ -6,27 +6,45 @@ require 'stud/interval'
 require 'csv'
 require_relative '../../logstash/Inputs/client_with_streaming_support'
 
-# TODO describe what class does
+
+# This plugin enables Salesforce customers to use ELK stack as there choice of exploration and visualization of their
+# EventLogFile(ELF) data from their Force.com ordinations.
+#
+# The plugin will handle downloading ELF CSV file and parsing them, any schema changes transparently, and event
+# deduplication.
 class LogStash::Inputs::SfdcElf < LogStash::Inputs::Base
 
   # Constants
   SEPARATOR = ','
   QUOTE_CHAR = '"'
   DEFAULT_TIME = '0001-01-01T00:00:00Z'
+  FILE_PREFIX = 'sfdc_elf_logstash'
+  LOG_KEY = 'SFDC'
 
   config_name 'sfdc_elf'
   default :codec, 'plain'
 
+  # Username to your Force.com organization.
   config :username, :validate => :string, :required => true
-  config :password, :validate => :password, :required => true       # todo might need to change validate to :password
-  config :security_token, :validate => :password, :required => true # todo might need to change validate to :password
+
+  # Password to your Force.com organization.
+  config :password, :validate => :password, :required => true
+
+  # Security token to you Force.com organization, can be found in My Settings > Personal > Reset My Security Token. Then
+  # it will take you to "Reset My Security Token" page, and click on the "Reset Security Token" button. The token will
+  # be emailed to you.
+  config :security_token, :validate => :password, :required => true
+
+  # The path to the .sfdc_info to use as an input. You set the path like so, `/var/log` Paths must be absolute and
+  # cannot be relative.
   config :sfdc_info_path, :validate => :string, :default => Dir.home
+
+  # How often this plugin should grab new data.
   config :poll_interval_in_hours, :validate => :number, :default => 24 #todo range, poll intervals 6 to 24
 
 
   public
   def register
-
     # Initaialize client
     @client = ClientWithStreamingSupport.new
     @client.client_id = '3MVG9xOCXq4ID1uGlgyzp8E4HEHfzI4iryotXS3FtHQIZ5VYhE8.JPehyksO.uYZmZHct.xlXVxqDCih35j0.'
@@ -36,118 +54,160 @@ class LogStash::Inputs::SfdcElf < LogStash::Inputs::Base
     # Authenticate the client
     authenticate
 
-    # Set up time interval for forever while loop
+    # Save org id to distinguish between multiple orgs.
+    #todo do i really need this?? only used it to create @path variable
+    # @org_id = @client.org_id #todo why doesnt this work???
+    @org_id = @client.query("select id from Organization")[0]["Id"]
+
+    # Set up time interval for forever while loop.
     # @interval = @poll_interval_in_hours * 3600
-    @interval = 60
+    @interval = 30
 
     # Set @sfdc_info_path to home directory if provided path from config does not exist.
     @sfdc_info_path = Dir.home unless File.directory?(@sfdc_info_path)
 
+    # Generate the path using org_id to keep track of the last read log file date based on the org rather than users.
+    @path = "#{@sfdc_info_path}/.#{FILE_PREFIX}_#{@org_id}"
+    @logger.info("#{LOG_KEY}: genarted path = #{@path}")
 
-    # Append .sfdc_info to the @sfdc_info_path.
-    @path = "#{@sfdc_info_path}/.sfdc_info"
-
-    # Read from .sfdc_info if it exists, otherwise create it.
+    # Read from .sfdc_info if it exists, otherwise load @last_read_log_date with DEFAULT_TIME.
     if File.exist?(@path)
-
       # Load last read LogDate from .sfdc_info.
-      @last_read_instant = File.read(@path)
-      @logger.info('MO: .sfdc_info does exist and read from it to @last_read_instant')
+      @last_read_log_date = File.read(@path)
+      @logger.info("#{LOG_KEY}: .#{@path} does exist, read time from it and stored it in @last_read_instant")
     else
 
       # Load default time to ensure getting all possible EventLogFiles from oldest to current.
-      # Note in run() .sfdc_info is created or overwritten, so no need to create it here.
-      @last_read_instant = DEFAULT_TIME
-
+      # Note in create_event_list(), which is called in run(), is where .sfdc_info is created or overwritten, so no need to create it here.
+      @last_read_log_date = DEFAULT_TIME
       @logger.info('MO: .sfdc_info does not exist and loaded DEFAULT_TIME to @last_read_instant')
     end
 
-    @logger.info('MO: @last_read_instant =  ' << @last_read_instant)
-  end # def register
+    @logger.info("#{LOG_KEY}: @last_read_instant =  #{@last_read_log_date}")
+  end
 
+  # def register
 
 
   public
   def run(queue)
-    begin
+    # Grab the current time and one @interval to it so that the while loop knows when it need to compute again.
+    next_schedule_time = Time.now + @interval
 
-      # next_schedule_time = Time.now
-      next_schedule_time = Time.now  + @interval
-
-      while true
+    while true
+      begin
+        # Line for readable log statements.
+        @logger.info('---------------------------------------------------')
 
         # Grab a list of Sobjects, specifically EventLogFiles.
-        query_result_list = @client.query("SELECT Id, EventType, Logfile, LogDate FROM EventLogFile WHERE LogDate > #{@last_read_instant} ORDER BY LogDate DESC ")
+        query_result_list = @client.query("SELECT Id, EventType, Logfile, LogDate FROM EventLogFile WHERE LogDate > #{@last_read_log_date} ORDER BY LogDate DESC ")
 
         if !query_result_list.empty?
-          @logger.info('MO: query result is NOT empty, size = ' << query_result_list.size.to_s)
+          @logger.info("#{LOG_KEY}: query result is NOT empty, size = #{query_result_list.size.to_s}")
 
-          #
-          @last_read_instant = Time.parse(query_result_list.first.LogDate.to_s).utc.iso8601.to_s
-
-          f = File.open(@path, 'w')
-          f.write(@last_read_instant)
-          f.flush
-          f.close
-
-          # Grab a list of Tempfiles that contains CSV file data.
-          tempfile_list = get_csv_files(query_result_list)
-
-          tempfile_list.each do |tmp|
-
-            # Get the column from Tempfile, which is in the first line and in CSV format, then parse it. It will return an array.
-            column = CSV.parse_line(tmp.readline, :col_sep => SEPARATOR, :quote_char => QUOTE_CHAR)
-
-            tmp.each_line do |data|
-              # Parse the current line, it will return an array.
-              parsed_data = CSV.parse_line(data, :col_sep => SEPARATOR, :quote_char => QUOTE_CHAR)
-
-              # create_event will return a event object.
-              queue << create_event(column, parsed_data)
-            end
-
-            # Close tmp file and unlink it, doing this will delete the actual tempfile.
-            tmp.close
-            tmp.unlink
-          end # do
-
+          # Grab an list of events based on the query result. Then simply append the events to the queue.
+          @logger.info('MO: going into create_event_list')
+          event_list = create_event_list(query_result_list)
+          @logger.info('MO: going into create_event_list')
+          event_list.each do |event| queue << event end
         else
-          @logger.info('MO: query result is empty')
+          @logger.info("#{LOG_KEY}: query result is empty")
+          save_last_read_log_date(Time.now.utc.iso8601.to_s)
         end
 
-        # Stall ...
-        @logger.info('MO: next_schedule_time = ' << next_schedule_time.to_s)
-        next_schedule_time = determine_loop_stall(next_schedule_time)
+        # Depending on the next_schedule_time and the time taking the compute the code above, sleep this loop and adjust the next_schedule_time.
+        @logger.info("#{LOG_KEY}: next_schedule_time = #{next_schedule_time.to_s}")
+        next_schedule_time = stall_schedule(next_schedule_time)
 
-      end # while loop
+      rescue Databasedotcom::SalesForceError => e
 
-    rescue Databasedotcom::SalesForceError => e
+        # Session has expired. Force user logout. Then re-authenticate
+        if e.message == 'Session expired or invalid'
+          @logger.info("#{LOG_KEY}: Session expired or invalid, authenticating again")
+          authenticate
 
-      # Session has expired. Force user logout. Then re-authenticate
-      if e.message == 'Session expired or invalid'
-        authenticate
-        @logger.info('MO: SalesForceError, authenticating again')
-      else
-        @logger.warn('MO: '  << e.message)
-        raise e
-      end
+          # todo adjust time???
+        else
+          @logger.error("SFDC: #{e.message}")
+        end
 
-    end # rescue / begin
-  end # def run
+      end # rescue / begin
+    end # while loop
+  end
 
-
+  # def run
 
 
   # This helper method is called whenever initaialize the client object or whenever the
-  # client token expires.
+  # client token expires. It will attempt 3 times with a 30 second delay between each retry.
 
   private
   def authenticate
-    @client.authenticate username: @username, password: @password.value + @security_token.value
-    @logger.info('MO: client has been authenticated')
+    @logger.info("#{LOG_KEY}: tyring to authenticate client")
+    3.times do |count|
+      begin
+       # If exception is not thrown, then break out of loop.
+        @client.authenticate username: @username, password: @password.value + @security_token.value
+        @logger.info("#{LOG_KEY}: client has been authenticated")
+        break
+      rescue Exception => e
+        # Sleep for 30 seconds.
+        unless (count == 2)
+          @logger.error("#{LOG_KEY}: Failed to authenticate going to try again in 30 seconds")
+          sleep(30)
+        else
+          raise e
+        end
+      end
+    end
   end # def authenticate
 
 
+  # Given a list of query result that are Sobjects, iterate through the list and grab all the CSV files that each
+  # Sobject points to via get_csv_tempfile_list(). Once that is done we save the first LogDate in the list to the
+  # @path file. After that we parse the CSV files parse it line by line and generate the events for the parsed CSV
+  # line, append it to a list and the finally return the event_list.
+  #
+  # Note: when grabbing the CSV files, they are stored as Tempfiles and deleted after parsed.
+
+  private
+  def create_event_list(query_result_list)
+    event_list = []
+
+    # query_result_list is in descending order based on the LogDate, so grab the first one of the list and save the LogDate to @last_read_log_date and .sfdc_info
+    # @last_read_log_date = Time.parse(query_result_list.first.LogDate.to_s).utc.iso8601.to_s
+    @last_read_log_date = Time.parse(query_result_list.first.LogDate.to_s).utc.iso8601.to_s
+
+    # Overwrite the .sfdc_elf_logstash file with the @last_read_log_date.
+    save_last_read_log_date(@last_read_log_date)
+
+    # Grab a list of Tempfiles that contains CSV file data.
+    tempfile_list = get_csv_tempfile_list(query_result_list)
+
+    # Loop though each tempfile.
+    tempfile_list.each do |tmp|
+
+      # Get the column from Tempfile, which is in the first line and in CSV format, then parse it. It will return an array.
+      column = CSV.parse_line(tmp.readline, :col_sep => SEPARATOR, :quote_char => QUOTE_CHAR)
+
+      # Loop through tempfile, line by line.
+      tmp.each_line do |data|
+
+        # Parse the current line, it will return an array.
+        parsed_data = CSV.parse_line(data, :col_sep => SEPARATOR, :quote_char => QUOTE_CHAR)
+
+        # create_event will return a event object.
+        event_list << create_event(column, parsed_data)
+      end
+
+      # Close tmp file and unlink it, doing this will delete the actual tempfile.
+      tmp.close
+      tmp.unlink
+    end # do loop, tempfile_list
+
+    # Return event_list
+    event_list
+  end # def create_event_list
 
 
   # This helper method takes as input a key data and val data that is in CSV format. Using
@@ -156,7 +216,6 @@ class LogStash::Inputs::SfdcElf < LogStash::Inputs::Base
   # return it.
   #
   # TODO: event.timestamp     = Time.strptime(values[i], 'YYYYMMddHHmmss.SSS').utc.iso8601
-
 
   private
   def create_event(column, data)
@@ -171,7 +230,7 @@ class LogStash::Inputs::SfdcElf < LogStash::Inputs::Base
       column_name = column[i]
 
       # Handle when field_name is 'TIMESTAMP', Change the @timestamp field to the actual time on the CSV file, but convert it to iso8601.
-      event.timestamp = Time.parse(data[i]).utc.iso8601 if column_name == 'TIMESTAMP'
+      event.timestamp = Time.parse(data[i]).utc.iso8601 if column_name == 'TIMESTAMP' #TODO check to see if we can add mill
 
       # Add the column data pair to event object.
       event[column_name] = data[i]
@@ -180,8 +239,6 @@ class LogStash::Inputs::SfdcElf < LogStash::Inputs::Base
     # Return the event
     event
   end # def create_event
-
-
 
 
   # This helper method takes as input a list/collection of Sobjects which each
@@ -197,17 +254,15 @@ class LogStash::Inputs::SfdcElf < LogStash::Inputs::Base
   # Note: for debugging tmp.path will help find the path where the Tempfile is stored. #
 
   private
-  def get_csv_files(query_result_list)
+  def get_csv_tempfile_list(query_result_list)
     #todo add try catch
     #todo isolate failes from one another, example large file
 
     result =[]
     query_result_list.each do |event_log_file|
 
-      # Create Tempfile and write the body of the http_result, which contains the csv data, to a buffer.
-      tmp = Tempfile.new('sfdc_elf')
-
-      # Get the path of the CSV file from the LogFile field, then do http get.
+      # Get the path of the CSV file from the LogFile field, then stream the data to the .write method of the Tempfile
+      tmp = Tempfile.new('sfdc_elf_tempfile')
       @client.streaming_download(event_log_file.LogFile, tmp)
 
       # Flushing will write the buffer into the Tempfile itself.
@@ -230,30 +285,40 @@ class LogStash::Inputs::SfdcElf < LogStash::Inputs::Base
 
   end # def get_csv_files
 
+  # Take as input a time sting that is in iso8601 format. The overwrite .sfdc_elf_logstash with the time string,
+  # because of the 'w' flag.
 
   private
-  def determine_loop_stall(next_schedule_time)
+  def save_last_read_log_date(time)
+    @logger.info("#{LOG_KEY}: overwriting #{@path} with #{time}")
+    f = File.open(@path, 'w')
+    f.write(time)
+    f.flush
+    f.close
+  end
+
+
+  private
+  def stall_schedule(next_schedule_time)
     current_time = Time.now
 
-    @logger.info('MO: time before sleep = ' << current_time.to_s)
+    @logger.info("#{LOG_KEY}: time before sleep  = #{current_time.to_s}")
 
     if current_time > next_schedule_time
-      @logger.info('MO: missed next schedule time, proceeding to next task without sleeping')
+      @logger.info("#{LOG_KEY}: missed next schedule time, proceeding to next task without sleeping")
       while current_time > next_schedule_time
         next_schedule_time += @interval
       end
     else
-      @logger.info('MO: sleeping for ' << (next_schedule_time - current_time).to_s)
+      @logger.info("#{LOG_KEY}: sleeping for #{(next_schedule_time - current_time).to_s} seconds")
       sleep(next_schedule_time - current_time)
       next_schedule_time += @interval
 
     end
-    @logger.info('MO: time after sleep = ' << Time.now.to_s)
+    @logger.info("#{LOG_KEY} time after sleep   = #{Time.now.to_s}")
 
     next_schedule_time
   end # def determine_loop_stall
-
-
 
 
 end # class LogStash::Inputs::File
